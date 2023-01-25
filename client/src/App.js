8f3249005e40bd64a7343cc2a40ae7359f1d1376
Sandivsv <sandeepkmr7379@gmail.com>
2023-01-25 11:59:19 +0530

    server connection establish with react to satrt

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import socketIO from "socket.io-client";
+import './App.css';
+
+const ENDPOINT =  "http://localhost:4500/";
+const socket = socketIO(ENDPOINT, {transports: ["websocket"]});
+
+
+
+function App() {
+
+  socket.on("connect", ()=>{
+    
+  })
+
+
+
+  return (
+    <div className="App">
+      <h1>working</h1>
+    </div>
+  );
+}
+
+export default App;
