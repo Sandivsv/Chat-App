5bdb25d4dcb5cfb57b4dce6e00662dd4867ad218
Sandivsv <sandeepkmr7379@gmail.com>
2023-01-26 16:24:54 +0530

    Implemented all sections

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Join from "./component/join/join";
 import Chat from "./component/chat/chat"
 
@@ -10,14 +10,12 @@ function App() {
 
 
   return (
-
-
-    <Router>
-      {/* <Join></Join> */}
-      <Chat></Chat>
-      {/* <Route exact path="/" component={Join} />
-      <Route path="/chat" component={Chat} /> */}
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Join/>}/>
+        <Route path="/chat" element={<Chat/>}/>
+      </Routes>
+    </BrowserRouter>
 
   );
 }
--- client/src/component/chat/chat.js
+++ client/src/component/chat/chat.js
@@ -1,34 +1,88 @@
 import React, { useEffect, useState } from 'react'
 import { user } from "../join/join";
+import socketIo from "socket.io-client";
 import "./chat.css";
 import sendLogo from "../../images/send.png";
+import Message from "../message/message";
 import closeIcon from "../../images/closeIcon.png";
 
+let socket;
 
-const ENDPOINT = "http://localhost:4500/";
+const ENDPOINT = "https://chat-backend-uzqx.onrender.com/";
+
+const Chat = () => {
+    const [id, setid] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const send = () => {
+        const message = document.getElementById('chatInput').value;
+        socket.emit('message', { message, id })
+        document.getElementById('chatInput').value = "";
+    }
+
+    // console.log(messages);
+    
+
+    useEffect(() => {
+        socket = socketIo(ENDPOINT, { transports: ['websocket'] });
+
+        socket.on('connect', () => {
+            setid(socket.id);
+        });
+
+        socket.emit('joined', { user })
+
+        socket.on('welcome', (data) => {
+            setMessages([...messages, data]);
+        })
+
+        socket.on('userJoined', (data) => {
+            setMessages([...messages, data]);
+        })
+
+        socket.on('leave', (data) => {
+            setMessages([...messages, data]);
+        })
+
+        return () => {
+            // socket.emit('disconnect');
+            socket.off();
+        }
+
+    }, [])
+
+    useEffect(() => {
+        socket.on('sendMessage', (data) => {
+            setMessages([...messages, data]);
+        })
+        return () => {
+            socket.off();
+        }
+    }, [messages])
 
 
 
-const chat = () => {
-
-
-
-  return (
-    <div className="chatPage">
+    return (
+        <div className="chatPage">
             <div className="chatContainer">
                 <div className="header">
                     <h2>C CHAT</h2>
                     <a href="/"> <img src={closeIcon} alt="Close" /></a>
                 </div>
-              
+                
+
+                <div className='chatBox'>
+                    {messages.map((item, i) => <Message user={item.id === id ? '' : item.user} message={item.message} classs={item.id === id ? 'right' : 'left'} />)}
+                </div>
+                
                 <div className="inputBox">
-                    <input  type="text" id="chatInput" />
-                    <button  className="sendBtn"><img src={sendLogo} alt="Send" /></button>
+                    <input type="text" id="chatInput" />
+                    <button onClick={(send)} className="sendBtn"><img src={sendLogo} alt="Send" /></button>
                 </div>
             </div>
 
         </div>
-  )
+    )
 }
 
-export default chat
+export default Chat
